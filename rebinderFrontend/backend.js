039d3cc48b72aeb1859c2760d403e0d311223a01
Bpetiii <biropeter2008@gmail.com>
2025-05-31 21:05:06 +0200

    basic popup mechanic

--- rebinderFrontend/backend.js
+++ rebinderFrontend/backend.js
@@ -9,7 +9,12 @@ function BackendAction(address, type) {
 window.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("add-scenario")
-    .addEventListener("click", AddScenario());
+    .addEventListener("click", AddScenario);
 });
 
-function AddScenario() {}
+const scenarioHolder = document.getElementById("scenario-holder");
+const scenarioPopup = document.getElementById("add-scenario-popup");
+
+function AddScenario() {
+  scenarioPopup.style.visibility = "visible";
+}
